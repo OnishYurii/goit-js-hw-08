fcf8341680b8dd6b1738f831724c716625bde7f7
OnishYurii <onishchenkoyurii@gmail.com>
2023-10-20 13:29:27 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onTime = data => {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', throttle(onTime, 1000));
+
+const currentTime = Number(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
